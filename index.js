a3687912a3bc8e73f6d1fcffd7cc4757bf583b88
Sean Oughton <seanoughton@gmail.com>
2018-06-28 16:29:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,3 +27,7 @@ function BoardMember(name, homeState, training){
    this.homeState = homeState
    this.training = training
  };
+
+ BoardMember.prototype.veto = function(){
+      return 'No, I must disagree'
+    }
